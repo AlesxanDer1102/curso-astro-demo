1e2ea2b78454373889abe2165fc7f36a660f1398
AlesxanDer1102 <diego.orrego.t@uni.pe>
2024-10-24 00:33:55 -0500

    add build of project

---
+++ .vercel/output/functions/_render.func/.vercel/output/_functions/entry.mjs
@@ -0,0 +1,34 @@
+import { renderers } from './renderers.mjs';
+import { c as createExports } from './chunks/entrypoint_CGfnr6A-.mjs';
+import { manifest } from './manifest_D1JL4KYj.mjs';
+
+const _page0 = () => import('./pages/_image.astro.mjs');
+const _page1 = () => import('./pages/404.astro.mjs');
+const _page2 = () => import('./pages/about.astro.mjs');
+const _page3 = () => import('./pages/blog/mi-primer-blog.astro.mjs');
+const _page4 = () => import('./pages/launch/_id_.astro.mjs');
+const _page5 = () => import('./pages/index.astro.mjs');
+
+const pageMap = new Map([
+    ["node_modules/astro/dist/assets/endpoint/generic.js", _page0],
+    ["src/pages/404.astro", _page1],
+    ["src/pages/about.astro", _page2],
+    ["src/pages/blog/mi-primer-blog.md", _page3],
+    ["src/pages/launch/[id].astro", _page4],
+    ["src/pages/index.astro", _page5]
+]);
+const serverIslandMap = new Map();
+const _manifest = Object.assign(manifest, {
+    pageMap,
+    serverIslandMap,
+    renderers,
+    middleware: () => import('./_noop-middleware.mjs')
+});
+const _args = {
+    "middlewareSecret": "47d9cf25-87d0-4200-9b4f-2c07e58e0908",
+    "skewProtection": false
+};
+const _exports = createExports(_manifest, _args);
+const __astrojsSsrVirtualEntry = _exports.default;
+
+export { __astrojsSsrVirtualEntry as default, pageMap };
